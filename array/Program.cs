a37898cfcfa6dd63f6f12e87fcc3e256201fdf46
wilhelmar <wilhelmar@vgy.se>
2016-10-21 15:15:19 +0200

    övning 1-3

--- array/Program.cs
+++ array/Program.cs
@@ -10,6 +10,73 @@ namespace array
     {
         static void Main(string[] args)
         {
+            /*
+            //Övning 1
+            Console.WriteLine("Välkommen!");
+            Console.WriteLine("Övning 1");
+            string[] namn = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Mata in ett namn: ");
+                namn[i] = Console.ReadLine();
+            }
+            Console.Write("Dina Vänner: ");
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.Write(namn [i] + ", ");
+               
+            }
+
+            Console.ReadLine();
+
+            //Övning 2
+
+            Console.WriteLine("Övning 2");
+
+            int[] siffror = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Mata in ett heltal: ");
+                siffror[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Dina tal baklänges:  ");
+            for (int x = 9; x >=0; x--)
+            {
+                Console.Write(siffror[x] + ", ");
+            }
+
+            Console.ReadLine();
+            */
+            //Övning 3
+
+            Console.WriteLine("Övning 3");
+
+            int[] numbArray = { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+
+            for (int i = 0; i < numbArray.Length; i++)
+            {
+                for (int x = 0; x < numbArray.Length; x++)
+                {
+                    if (numbArray[i] < numbArray[x])
+                    {
+                        int temp = numbArray[x];
+                        numbArray[x] = numbArray[i];
+                        numbArray[i] = temp;
+
+                    }
+                }
+                Console.WriteLine(numbArray[]);
+            }
+
+        
+
+
+
+
+
+
+
+
         }
     }
 }
